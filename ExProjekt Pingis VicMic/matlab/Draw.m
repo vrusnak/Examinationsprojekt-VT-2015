a2517f664542fe2c1dea43c059090cfb6bd64400
vrusnak <viktorrusnak10@gmail.com>
2015-08-27 12:30:55 +0200

    Update Draw.m

--- ExProjekt Pingis VicMic/matlab/Draw.m
+++ ExProjekt Pingis VicMic/matlab/Draw.m
@@ -6,6 +6,7 @@
 N=200;
 doubleValueFirst = zeros(1, N);
 doubleValueSecond = zeros(1, N);
+doubleValueThird = zeros(1, N);
 t = zeros(1, N);
 
 flushinput(s);
@@ -14,13 +15,15 @@ flushinput(s);
       t(i)= i;
       inValue1 = fscanf(s);
       inValue2 = fscanf(s);
+      inValue3 = fscanf(s);
       doubleValueFirst(i) = str2double(inValue1);
-	  doubleValueSecond(i) = str2double(inValue2);
+      doubleValueSecond(i) = str2double(inValue2);
+      doubleValueThird(i) = str2double(inValue3);
       
-      plot(t,doubleValueFirst,'r--', t,doubleValueSecond,'b--');
-      title('Styrsignal och objektavstÂnd')
+      plot(t,doubleValueFirst,'g--', t,doubleValueSecond,'b--', t,doubleValueThird,'r--');
+      title('Styrsignal och objektavst√•nd')
       xlabel('Sampel')
-      ylabel('V‰rden')
-      legend('Styrv‰rde', 'ObjektavstÂnd')
+      ylabel('V√§rden')
+      legend('Styrv√§rde', 'Objektavst√•nd')
       drawnow;
   end
